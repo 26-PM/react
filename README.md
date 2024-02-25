7d26811a4e9e0c72adc5af57cbffcb31c523c396
PM <121531128+26-PM@users.noreply.github.com>
2024-02-25 10:32:16 +0000

    implemented react router :)

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # react
 Heya!
+
+# Hooks Effects and Components
+
+# React Routers
+
+# npm i -D react-router-dom
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+# const router=createBrowserRouter([
+    {
+    path:"/",
+    element:<><Navbar/><Home/></>
+    },
+    {
+      path:"/login",
+      element:<><Navbar/><Login/></>
+    },
+    {
+      path:"/about",
+      element:<><Navbar/><About/></>
+    },
+    {
+      path:"/user/:username",
+      element:<><Navbar/><User/></>
+    },
+])
+
+<RouterProvider router={router}></RouterProvider>
+
+
--- hooksEffectsAndComponents/src/App.jsx
+++ hooksEffectsAndComponents/src/App.jsx
@@ -1,7 +1,32 @@
 import { useState,useEffect,useRef } from 'react'
+import Navbar from './components/Navbar'
 import './App.css'
-
+import { BrowserRouter, Routes, Route, createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from './components/Home';
+import Login from './components/Login';
+import About from './components/About';
+import User from './components/User';
 function App() {
+
+  const router=createBrowserRouter([
+    {
+    path:"/",
+    element:<><Navbar/><Home/></>
+    },
+    {
+      path:"/login",
+      element:<><Navbar/><Login/></>
+    },
+    {
+      path:"/about",
+      element:<><Navbar/><About/></>
+    },
+    {
+      path:"/user/:username",
+      element:<><Navbar/><User/></>
+    },
+])
+
   const [count, setCount] = useState(0)
 
   // Use ref
@@ -34,9 +59,9 @@ function App() {
 
 
 
-
   return (
     <>
+    <RouterProvider router={router}></RouterProvider>
     <div className="counter">
       Value of counter = {count}
     </div>
